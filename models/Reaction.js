ebb8a9625a7c1258803ca4f65195a330259c1048
Soleiles <ktaing789@gmail.com>
2023-08-17 13:30:28 -0400

    updated reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,42 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 200
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate,
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    },
+);
+
+function formatDate(createdAt) {
+    return createdAt.toLocalDateString("en-US", {
+        day: "2-digit",
+        year: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+};
+
+module.exports = reactionSchema;
