0f5bae2b36008b1cf34d1bfe0e703b8ffb87e8ba
Soleiles <ktaing789@gmail.com>
2023-08-17 19:09:22 -0400

    updated thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+// Calls all functions from thoughtController
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+// Manages all thought functions
+router.route('/').get(getThoughts).post(createThought);
+router  
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// Manages reaction functions
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
