138fb36a02c38313f92b9578d0f7df27c2926887
Soleiles <ktaing789@gmail.com>
2023-08-17 13:44:03 -0400

    added GET, POST, and PUT to userController

---
+++ controllers/userController.js
@@ -0,0 +1,58 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Gets all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    // Gets user by ID
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({_id: req.params.userId})
+                .select('-__v')
+                .populate('thoughts')
+                .populate('friends');
+            if(!user) {
+                return res.status(404).json({message: 'No user with that ID'})
+            }
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // Posts new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    // Updates a user
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$set: req.body},
+                {runValidators: true, new: true}
+            );
+            if(!user) {
+                return res.status(404).json({message: 'No user with that ID'});
+            };
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+}
