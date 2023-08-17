f998b7a2e890aa99a41d3496394f5352cd3b4917
Soleiles <ktaing789@gmail.com>
2023-08-17 18:39:58 -0400

    added POST and PUT in thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought, User} = require('../models');
+const { Thought, User } = require('../models');
 
 module.exports = {
     // GET all thoughts
@@ -14,13 +14,51 @@ module.exports = {
     // GET thought by ID
     async getSingleThought(req, res) {
         try {
-            const thought = await Thought.findOne({_id: req.params.thoughtId})
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
                 .select('-__v');
-            if(!thought) {
-                return res.status(404).json({message: 'No thought with that ID'});
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
             }
             res.json(thought);
         } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // POST new thought
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thought._id } },
+                { new: true }
+            )
+            if (!user) {
+                return res.status(404).json({
+                    message: 'No user with that ID',
+                })
+            }
+            res.json('New thought created');
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    // Update thought (PUT)
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if(!thought) {
+                return res.status(404).json({message: 'No thought with that ID'});
+            };
+            res.json(thought);
+        } catch (err){
             console.log(err);
             res.status(500).json(err);
         }
